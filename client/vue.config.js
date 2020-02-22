4fb60bf8c44bff17265f2515f5cf2b6787c6fe94
Hamza Mogni <hamzamogni5@gmail.com>
2020-02-22 14:10:50 +0100

    configured build path of vue CLI to public dir of server

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  lintOnSave: false
+  lintOnSave: false,
+  outputDir: "../server/public/",
+  indexPath: '../resources/views/index.blade.php'
 }
