f6df095da4b3d9807ded4f12a5fc7c7282f8d0d8
Hamza Mogni <hamzamogni5@gmail.com>
2020-03-06 21:30:12 +0100

    added meetings to store root object

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import cells from "./modules/cells";
 import members from "./modules/members";
+import meetings from "./modules/meetings";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         cells,
-        members
+        members,
+        meetings,
     },
 })
