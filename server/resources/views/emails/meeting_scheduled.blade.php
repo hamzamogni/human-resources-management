8af831500951bf47c9a147b4e7798624413902b3
Hamza Mogni <hamzamogni5@gmail.com>
2020-03-06 21:03:50 +0100

    added meeting scheduled email view

---
+++ server/resources/views/emails/meeting_scheduled.blade.php
@@ -0,0 +1,11 @@
+Hello {{ $member->full_name }}, <br>
+
+As a member of <strong>{{ $cell->name }}</strong> cell, you have a meeting scheduled on {{ $meeting->meeting_date }}
+at {{ $meeting->meeting_time }}. <br>
+
+In case you won't be able to join the meeting, please contact the Human resources responsible at rh@ecdh.ma, <br><br>
+
+NB: Do not in any case reply to this email, it won't be received by anyone. <br><br>
+
+Best Regards. <br>
+ECDH Platform
